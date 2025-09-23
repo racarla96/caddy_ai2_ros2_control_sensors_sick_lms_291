dbbb024c91f74133586962727c04c6bc403d62c7
Rafael Carbonell Lázaro <61716519+racarla96@users.noreply.github.com>
2025-09-23 22:07:54 +0200

    Update udev rules filename for sick device

--- startup/initenv.sh
+++ startup/initenv.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo  'KERNEL=="ttyUSB*", ATTRS{idVendor}=="067b", ATTRS{idProduct}=="2303", MODE:="0666", GROUP:="dialout",  SYMLINK+="sick"' > /etc/udev/rules.d/sick.rules
+echo  'KERNEL=="ttyUSB*", ATTRS{idVendor}=="067b", ATTRS{idProduct}=="2303", MODE:="0666", GROUP:="dialout",  SYMLINK+="sick"' > /etc/udev/rules.d/ros2-sensor-usb-sick.rules
 
 service udev reload
 sleep 2
